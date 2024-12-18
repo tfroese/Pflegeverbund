0db1bb9dfb32dcc73b391eac2277c3662e82d989
tfroese <tfroese@me.com>
2024-12-18 17:56:16 +0100

    Update

--- components/top-bar.php
+++ components/top-bar.php
@@ -1,6 +1,6 @@
 <div class="top-bar">
-    <div class="container">
-        <a href="https://www.bundesgesundheitsministerium.de/service/publikationen/details/pflegeleistungen-zum-nachschlagen.html" target="_blank" class="top-bar__blog-link">Bundesministerium für Gesundheit Artikel über Pflegestufen</a>
+    <div class="top-bar__container">
+        <a href="https://www.bundesgesundheitsministerium.de/service/publikationen/details/pflegeleistungen-zum-nachschlagen.html" target="_blank" class="top-bar__blog-link">Bundesministerium für Gesundheit Artikel über Pflegestufen →</a>
         <span class="top-bar__date">Stand: 18. Dezember 2024</span>
     </div>
 </div>
