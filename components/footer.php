32b01a9fa1d6cc2d62a7206c95e59c0a2a115105
tfroese <tfroese@me.com>
2024-12-18 10:18:10 +0100

    Update

--- components/footer.php
+++ components/footer.php
@@ -7,9 +7,9 @@
             <div class="footer__column">
                 <h3>Rechtliche Links</h3>
                 <div class="footer__links">
-                    <a href="/agb">Allgemeine Geschäftsbedingungen</a>
-                    <a href="/datenschutz">Datenschutzerklärung</a>
-                    <a href="/impressum">Impressum</a>
+                    <a href="<?=SITE_PATH;?>/agb">Allgemeine Geschäftsbedingungen</a>
+                    <a href="<?=SITE_PATH;?>/datenschutz">Datenschutzerklärung</a>
+                    <a href="<?=SITE_PATH;?>/impressum">Impressum</a>
                 </div>
             </div>
             
@@ -17,15 +17,15 @@
                 <h3>Nützliche Links</h3>
                 <div class="footer__links">
                     <a href="https://www.bundesgesundheitsministerium.de/themen/pflege/pflegegrade" target="_blank" rel="noopener">BMG: Pflegestufen</a>
-                    <a href="/faq">Häufig gestellte Fragen</a>
+                    <a href="<?=SITE_PATH;?>/faq">Häufig gestellte Fragen</a>
                 </div>
             </div>
             
             <div class="footer__column">
                 <h3>Häufige Fragen</h3>
                 <div class="footer__links">
-                    <a href="/faq/pflegeleistungen">Stehen mir Pflegeleistungen zu?</a>
-                    <a href="/faq/pflegedienst">Wie kann ich die Leistungen des Pflegedienstes in Anspruch nehmen?</a>
+                    <a href="<?=SITE_PATH;?>/faq/pflegeleistungen">Stehen mir Pflegeleistungen zu?</a>
+                    <a href="<?=SITE_PATH;?>/faq/pflegedienst">Wie kann ich die Leistungen des Pflegedienstes in Anspruch nehmen?</a>
                 </div>
             </div>
         </div>
