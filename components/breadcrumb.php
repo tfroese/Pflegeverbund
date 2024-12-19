10627febad4cf41e1f7ef919686de32343679b13
tfroese <tfroese@me.com>
2024-12-19 07:39:07 +0100

    Update

--- components/breadcrumb.php
+++ components/breadcrumb.php
@@ -1,9 +1,21 @@
 <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <?php foreach ($items as $item): ?>
-            <li class="breadcrumb-item">
-                <a href="<?= htmlspecialchars($item['url']) ?>"><?= htmlspecialchars($item['label']) ?></a>
-            </li>
-        <?php endforeach; ?>
-    </ol>
-</nav>
+        <ol class="breadcrumb">
+            <?php 
+            $lastIndex = count($items) - 1;
+            foreach ($items as $index => $item): 
+                $isLast = $index === $lastIndex;
+            ?>
+                <li class="breadcrumb-item">
+                    <?php if (!$isLast): ?>
+                        <a href="<?= htmlspecialchars($item['url']) ?>">
+                            <?= htmlspecialchars($item['label']) ?>
+                        </a>
+                    <?php else: ?>
+                        <span class="breadcrumb-current">
+                            <?= htmlspecialchars($item['label']) ?>
+                        </span>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ol>
+    </nav>
